55c7c986a86f8f979bd0ec25a84ecb3288cf247f
Khushi Dayal <khushidayal25.94@gmail.com>
2025-06-30 22:45:31 +0800

    update user seeder with correct details.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,19 +15,27 @@ class UserSeeder extends Seeder
         $seedUsers = [
             [
                 'name' => 'Santina Deckow',
+                'given_name' => 'Santina',
+                'family_name' => 'Deckow',
                 'email' => 'tdurgan@example.net',
                 'password' => 'password',
                 'email_verified_at' => now(),
             ],
             [
                 'name' => 'Reynold Bednar',
+                'given_name' => 'Reynold',
+                'family_name' => 'Bednar',
                 'email' => 'kaela.jacobson@example.com',
                 'password' => 'password',
+                'email_verified_at' => null,
             ],
             [
                 'name' => 'Mrs. Stacy Rolfson',
+                'given_name' => 'Stacy',
+                'family_name' => 'Rolfson',
                 'email' => 'athena19@example.org',
                 'password' => 'password',
+                'email_verified_at' => null,
             ],
         ];
 
