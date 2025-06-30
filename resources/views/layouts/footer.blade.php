2603419286f9b7e2e74c0f233fe026b291cc60c4
Khushi Dayal <khushidayal25.94@gmail.com>
2025-06-30 22:57:42 +0800

    changes related to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,18 +4,18 @@
                text-sm
                grid grid-cols-1 md:grid-cols-2 gap-2">
     <section class="">
-        <p>&copy; Copyright YYYY COMPANY/YOURNAME. All rights reserved.</p>
+        <p>&copy; Copyright 2024 Khushboo Dayal. All rights reserved.</p>
     </section>
     <section class="grid grid-cols-2 gap-4">
         <nav class="flex flex-col gap-1">
-            <a href="{{ route('home') }}">Home</a>
-            <a href="{{ route('home') }}">About</a>
-            <a href="{{ route('home') }}">Contact Us</a>
+            <a href="{{ route('home') }}" class="hover:text-white">Home</a>
+            <a href="{{ route('about') }}" class="hover:text-white">About</a>
+            <a href="{{ route('contact-us') }}" class="hover:text-white">Contact Us</a>
+            <a href="{{ route('privacy') }}" class="hover:text-white">Privacy</a>
         </nav>
         <nav class="flex flex-col gap-1">
-            <a href="{{ route('home') }}">License</a>
-            <a href="{{ route('home') }}">Help</a>
-            <a href="{{ route('home') }}">Link X</a>
+            <a href="{{ route('pricing') }}" class="hover:text-white">Pricing</a>
+            <a href="#" class="hover:text-white">Terms of Service</a>
         </nav>
     </section>
 </footer>
